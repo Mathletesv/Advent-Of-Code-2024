303caf831a22b6d2b3c542fa64f297613cfb105f
Mathletesv <63653479+Mathletesv@users.noreply.github.com>
2024-12-13 18:41:11 +0000

    Way better Day 13 code

--- thirteen.py
+++ thirteen.py
@@ -3,45 +3,31 @@ import re
 with open("thirteen.txt", "r") as read:
 	data = read.read().split("\n\n")
 
+def parse(dataset):
+	return list(map(int, re.findall(r"[XY][+=](\d+)", dataset)))
+
+def solve_sys(v):
+	b = (v[4] / v[0] - v[5] / v[1]) / (v[2] / v[0] - v[3] / v[1])
+	a = (v[4] - v[2] * b) / v[0]
+	if abs(round(a) - a) < .001 and abs(round(b) - b) < .001:
+		return round(a) * 3 + round(b)
+	return 0
+
 def one():
 	total = 0
 	for dataset in data:
-		lines = dataset.split("\n")
-		lines[0] += 'end.'
-		lines[1] += 'end.'
-		lines[2] += 'end.'
-		x1 = int(re.search(r'X+(.*?),', lines[0]).group(1).lstrip('+'))
-		x2 = int(re.search(r'X+(.*?),', lines[1]).group(1).lstrip('+'))
-		y1 = int(re.search(r'Y+(.*?)end.', lines[0]).group(1).lstrip('+'))
-		y2 = int(re.search(r'Y+(.*?)end.', lines[1]).group(1).lstrip('+'))
-		rx = int(re.search(r'X=(.*?),', lines[2]).group(1).lstrip('='))
-		ry = int(re.search(r'Y=(.*?)end.', lines[2]).group(1).lstrip('='))
-		b = (rx / x1 - ry / y1) / (x2 / x1 - y2 / y1)
-		a = (rx - x2 * b) / x1
-		if abs(round(a) - a) < .001 and abs(round(b) - b) < .001:
-			total += round(a) * 3 + round(b)
+		v = parse(dataset)
+		total += solve_sys(v)
 	print(total)
 		
 
 def two():
 	total = 0
 	for dataset in data:
-		lines = dataset.split("\n")
-		lines[0] += 'end.'
-		lines[1] += 'end.'
-		lines[2] += 'end.'
-		x1 = int(re.search(r'X+(.*?),', lines[0]).group(1).lstrip('+'))
-		x2 = int(re.search(r'X+(.*?),', lines[1]).group(1).lstrip('+'))
-		y1 = int(re.search(r'Y+(.*?)end.', lines[0]).group(1).lstrip('+'))
-		y2 = int(re.search(r'Y+(.*?)end.', lines[1]).group(1).lstrip('+'))
-		rx = int(re.search(r'X=(.*?),', lines[2]).group(1).lstrip('='))
-		ry = int(re.search(r'Y=(.*?)end.', lines[2]).group(1).lstrip('='))
-		rx += 10000000000000
-		ry += 10000000000000
-		b = (rx / x1 - ry / y1) / (x2 / x1 - y2 / y1)
-		a = (rx - x2 * b) / x1
-		if abs(round(a) - a) < .001 and abs(round(b) - b) < .001:
-			total += round(a) * 3 + round(b)
+		v = parse(dataset)
+		v[4] += 10000000000000
+		v[5] += 10000000000000
+		total += solve_sys(v)
 	print(total)
 
 one()
